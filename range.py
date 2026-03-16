5f28a5580814e4150401fc55985b2f67fc345e30
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 14:21:05 -0400

    Agregar archivo range.py con implementación de la función range y ejemplos de uso

---
+++ range.py
@@ -0,0 +1,17 @@
+# funcion range ->sintaxis range(inicio,fin,incremento)
+
+print("Funcion Range")
+
+for i in range(5):
+  print(i, end = " ") #imprime del 0 al 4
+  
+print("\n\n\n")
+
+print("Secuencia del 10 al 20")
+for i in range (10,20+1):
+  print(i, end = " ") #imprime del 10 al 20
+  
+print("\n\n\n")
+print("Secuencia del 20 al 30 de 2 en 2")
+for i in range (20,30+1,2):
+  print(i, end = " ") #imprime del 20 al 30 de 2 en 2
