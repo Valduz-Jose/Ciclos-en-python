994c6a0a87526c024f1ec455cc7ed2966757ebc9
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 14:24:24 -0400

    Agregar archivo mensaje_range.py con implementación para repetir un mensaje un número específico de veces

---
+++ mensaje_range.py
@@ -0,0 +1,7 @@
+print("Repeticion de un mensaje")
+
+mensaje = input("Ingrese un mensaje: ")
+repeticiones = int(input("Ingrese el número de repeticiones: "))
+
+for _ in range(repeticiones):
+  print({mensaje})
