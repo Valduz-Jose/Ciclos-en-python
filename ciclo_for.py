23e9727c2a14ee69a2b71d656cf1e14fbec7b1bf
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 12:30:22 -0400

    Agregar archivo ciclo_for.py con implementación de ciclo for

---
+++ ciclo_for.py
@@ -0,0 +1,5 @@
+# Ciclo for ejecuta un bloque de código un número determinado de veces, a diferencia del ciclo while que se ejecuta mientras se cumpla una condición.
+print("Ciclo For")
+cadena = "Hola Mundo"
+for letra in cadena:
+  print(letra, end=" ")
